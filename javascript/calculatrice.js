bc3e08d7a87d05773b046e6607addfc5e42bbaa9
Alex <alexandre.pitarch@divcom.ch>
2024-03-08 23:21:58 +0100

    ajout des variables constantes javascript et du contenu sto1 2 et 3

--- javascript/calculatrice.js
+++ javascript/calculatrice.js
@@ -14,7 +14,17 @@ const chiffre3 = document.getElementById('3');
 const chiffre4 = document.getElementById('4');
 const chiffre5 = document.getElementById('5');
 const chiffre6 = document.getElementById('6');
-const chiffre7
-const chiffre8
-const chiffre9
-const chiffre0
+const chiffre7 = document.getElementById('7');
+const chiffre8 = document.getElementById('8');
+const chiffre9 = document.getElementById('9');
+const chiffre0 = document.getElementById('0');
+
+//Permet de convertir les chiffre en négatif/positif
+const negatifPositif = document.getElementById('+/-');
+
+//Permet de mettre une virgule dans le nombre
+const virguleJs = document.getElementById('virgule');
+
+//x
+
+
