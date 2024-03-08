ef553cf117fa7d1ab3aa3609013060970f7f9b63
Alex <alexandre.pitarch@divcom.ch>
2024-03-08 22:48:08 +0100

    création des chiffres en html

---
+++ javascript/calculatrice.js
@@ -0,0 +1,20 @@
+/**
+ * Calculatrice
+ * @author  Alexandre Pitarch
+ * @version 1.0
+ * @since   2024-03-08
+ */
+
+'use strict'; // Active le mode strict du JavaScript
+
+//Déclaration des chiffres
+const chiffre1 = document.getElementById('1');
+const chiffre2 = document.getElementById('2');
+const chiffre3 = document.getElementById('3');
+const chiffre4 = document.getElementById('4');
+const chiffre5 = document.getElementById('5');
+const chiffre6 = document.getElementById('6');
+const chiffre7
+const chiffre8
+const chiffre9
+const chiffre0
