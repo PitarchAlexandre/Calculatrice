48e934833efd2b229f35b214438b3bbde605810b
Alex <alexandre.pitarch@divcom.ch>
2024-03-10 22:43:53 +0100

    création de fenêtre confirm

---
+++ javascript/msgAccueil.js
@@ -0,0 +1,5 @@
+const message = 'Ce site n\'utilise pas encore de cookies, car je n\'ai pas encore les compétences informatiques pour ' +
+                'le faire. 😔\n' +
+                'Acceptes-tu de visiter mon site malgré mon incapacité actuelle à te piquer tes données ? 😉';
+
+confirm(message);
