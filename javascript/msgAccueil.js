b4b6eb822dd1afebefe178fec02621fe38fca4c4
Alex <alexandre.pitarch@divcom.ch>
2024-03-10 23:51:52 +0100

    ajout des icon

--- javascript/msgAccueil.js
+++ javascript/msgAccueil.js
@@ -1,4 +1,4 @@
-//déclaration de la variable qui affiche le message d'accueil
+
 const message = 'Ce site n\'utilise pas encore de cookies, car je n\'ai pas encore les compétences informatiques pour ' +
                 'le faire. 😔\n' +
                 'Acceptes-tu de visiter mon site malgré mon incapacité actuelle à te piquer tes données ? 😉';
