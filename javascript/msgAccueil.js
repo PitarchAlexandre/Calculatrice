d4b675612559744e525515a931fe8b4df54b732e
Alex <alexandre.pitarch@divcom.ch>
2024-03-10 22:59:26 +0100

    Optimisation message d'accueil

--- javascript/msgAccueil.js
+++ javascript/msgAccueil.js
@@ -1,6 +1,8 @@
+//déclaration de la variable qui affiche le message d'accueil
 const message = 'Ce site n\'utilise pas encore de cookies, car je n\'ai pas encore les compétences informatiques pour ' +
                 'le faire. 😔\n' +
                 'Acceptes-tu de visiter mon site malgré mon incapacité actuelle à te piquer tes données ? 😉';
 
-//permet de stocker la confirmation de
+//Permet de stocker le confirmation de l'utilisateur qu'aucunes données de l'utilisateur n'est utilisé
+//Affiche le message une fois que l'utilisateur entre sur le site
 const confirmation = confirm(message);
